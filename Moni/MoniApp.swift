d6365b830a61a536f54615fb1bb5a7bf3c8e4f6b
Seaony <seaony@outlook.com>
2026-08-26 15:40:16 +0800

    Initialize macOS app project

---
+++ Moni/MoniApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoniApp.swift
+//  Moni
+//
+//  Created by seaony on 2026/8/26.
+//
+
+import SwiftUI
+
+@main
+struct MoniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
